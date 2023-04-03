e12de5f988c03d34331d65f3ede0644b3976e9b7
Jacek Cielecki <jcielecki@sii.pl>
2023-04-03 15:22:09 +0200

    dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to my blog!';
+  const likes = 50;
+  const person = { name: 'jacek', age: 23 }
+  const link = 'https://www.google.pl/';
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ person.name }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+        <a href={ link }>Google site</a>
       </div>
     </div>
   );
