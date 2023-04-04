91dc8346548dfa44f8f515e7ca8aaf2eda569793
Jacek Cielecki <jcielecki@sii.pl>
2023-04-04 12:00:57 +0200

    Add styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>The Blog</h1>
             <div className="links">
                 <a href="/">Home</a> &nbsp;
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "8px"
+                }}>New Blog</a>
             </div>
         </nav>
      );
