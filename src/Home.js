a343285b802b5b587e4439db3c95466a243b665d
Jacek Cielecki <jcielecki@sii.pl>
2023-04-04 13:30:02 +0200

    useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
-const Home = () => {
-// e - event object
-    const handleClick = (e) => {
-        console.log('hello world', e)    
+import { useState } from "react";
+
+const Home = () => {  
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge(age + 1);
     }
+
+    // e - event object
     const handleClickAgain = (name, e) => {
         console.log('hello ' + name, e.target)    
     }
@@ -10,6 +17,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>Name: { name } Age: { age }</p>
             <button onClick={handleClick}>Click me!</button>
             <button onClick={(e) => {handleClickAgain('jacek', e)}}>Click me againg!</button>
         </div>   
