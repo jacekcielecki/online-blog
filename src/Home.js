568e8e8ca4f1dd5d65bf7d1918795ce0909cde73
Jacek Cielecki <jcielecki@sii.pl>
2023-04-04 12:59:12 +0200

    click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+// e - event object
+    const handleClick = (e) => {
+        console.log('hello world', e)    
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target)    
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me!</button>
+            <button onClick={(e) => {handleClickAgain('jacek', e)}}>Click me againg!</button>
         </div>   
      );
 }
