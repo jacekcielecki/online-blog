439b328cde3cb2a1d5276cef77fea818c25d5d22
Jacek Cielecki <jcielecki@sii.pl>
2023-04-04 14:53:35 +0200

    props

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {  
     const [name, setName] = useState('mario');
     const [age, setAge] = useState(25);
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        { title: 'Web dev tips', body: 'lorem ipsum...', author: 'mario', id: 3},
+    ])
 
     const handleClick = () => {
         setName('luigi');
@@ -20,6 +26,8 @@ const Home = () => {
             <p>Name: { name } Age: { age }</p>
             <button onClick={handleClick}>Click me!</button>
             <button onClick={(e) => {handleClickAgain('jacek', e)}}>Click me againg!</button>
+            <br></br>
+            <BlogList blogs={blogs} title="All posts"/>
         </div>   
      );
 }
