62e84cfc7008b3be4691b958f89a4ae321a9d07f
Jacek Cielecki <jcielecki@sii.pl>
2023-04-05 15:35:04 +0200

    add routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title = 'Welcome to my blog!';
-  const likes = 50;
-  const person = { name: 'jacek', age: 23 }
-  const link = 'https://www.google.pl/';
+  // const title = 'Welcome to my blog!';
+  // const likes = 50;
+  // const person = { name: 'jacek', age: 23 }
+  // const link = 'https://www.google.pl/';
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ person.name }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-        <a href={ link }>Google site</a>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+          {/* <h1>{ title }</h1>
+          <p>Liked { likes } times</p>
+          <p>{ person.name }</p>
+          <p>{ [1,2,3,4,5] }</p>
+          <p>{ Math.random() * 10 }</p>
+          <a href={ link }>Google site</a> */}
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Blog</h1>
             <div className="links">
-                <a href="/">Home</a> &nbsp;
-                <a href="/create" style={{
+                <Link to="/">Home</Link> &nbsp;
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
      );
 }
+
+//Use <Link> instead of <a> for SPA application (limit requests).
  
 export default Navbar;
