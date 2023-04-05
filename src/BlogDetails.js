2c768e47c5eacb565f383a7511f31063d359a405
Jacek Cielecki <jcielecki@sii.pl>
2023-04-05 16:33:35 +0200

    get blog by id

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,20 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams() //param from route
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && 
+            <article>
+                <h2>{ blog.title }</h2>
+                <p>Written by { blog.author }</p>
+                <div>{ blog.body }</div>
+            </article>}
         </div>
       );
 }
