dabf1e12fd3e5688805f4d2c33aa1bb1a923626e
Jacek Cielecki <jcielecki@sii.pl>
2023-04-05 15:51:54 +0200

    update useFetch hook add AbortController

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -4,10 +4,13 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
+
     //custom hooks in react need to have a name that starts with use-
     useEffect(() => {
+        const abortConst = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortConst.signal })
                 .then(res => {
                     if(!res.ok){
                         throw Error('could not fetch the data from db');
@@ -21,11 +24,17 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch(err => {
-                    console.log(err.message);
-                    setError(err.message);
-                    setIsPending(false);
+                    if (err.name === "AbortError"){
+                        console.log('fetch aborted')
+                    } else {
+                        console.log(err.message);
+                        setError(err.message);
+                        setIsPending(false);
+                    }
                 })
         }, 1000)//delay fetch data for 1 sec in order to demonstrate loading screen
+    
+        return () => abortConst.abort();
     }, [url]);
 
     return { data, isPending, error } //better to return obj instead of array beacause later order of properties does not matter when destruct
