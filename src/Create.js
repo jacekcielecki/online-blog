76a696f519bb7c7c1cb38988ab0b231422c0fa30
Jacek Cielecki <jcielecki@sii.pl>
2023-04-14 18:22:14 +0200

    add create form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+
     return (
         <div className="create">
             <h2>Add a new post</h2>
+            <form>
+                <label>Post title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                 />
+                <label>Post body:</label>
+                <input
+                    type="text"
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                 />
+                <label>Post author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
       );
 }
